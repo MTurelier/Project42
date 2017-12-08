ab3a03d06c3f887b18949f9b5db3739d4ecb83cc
MTurelier <turelier.mathieu@live.fr>
2017-12-08 15:13:30 +0100

    First Commit Origin

--- src/main/java/com/gr42/insta/rest/PublicationResourceRESTService.java
+++ src/main/java/com/gr42/insta/rest/PublicationResourceRESTService.java
@@ -18,6 +18,8 @@ package com.gr42.insta.rest;
 
 
 import com.gr42.insta.model.NewMediaRequest;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 
 import javax.enterprise.context.RequestScoped;
@@ -26,7 +28,12 @@ import javax.validation.Validator;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Logger;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 /**
  * JAX-RS Example
@@ -47,8 +54,21 @@ public class PublicationResourceRESTService {
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createPublication(@MultipartForm NewMediaRequest request) {
+    public Response createPublication(@MultipartForm MultipartFormDataInput request) {
         Response.ResponseBuilder builder = null;
+        NewMediaRequest req = new NewMediaRequest();
+        byte [] img = null;
+        try {
+            req.setComment(request.getFormDataMap().get("comment").get(0).getBodyAsString());
+            InputStream inputStream = request.getFormDataMap().get("image").get(0).getBody(InputStream.class, null);
+            if (inputStream.available() != 0)
+                img = IOUtils.toByteArray(inputStream);
+            FileUtils.writeByteArrayToFile(new File("pathname"), img);
+        } catch (IOException e) {
+            e.printStackTrace();
+            builder = Response.noContent();
+            return builder.build();
+        }
         builder = Response.ok();
         return builder.build();
     }
